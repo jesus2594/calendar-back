97896e9af9bbeac811cf01de7cb9d097fcb9a02a
Jesus <jesusdavid890hotmail.com>
2023-02-10 17:42:40 -0500

    Backend 1.0 terminado

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+
+console.log(process.env)
+
+//Crear Servidor de Express
+const app = express();
+
+//Base de datos
+dbConnection();
+
+//CORS
+app.use(cors())
+
+//Directorio publico
+app.use(express.static('public'));
+
+//Lectura y parseo del body
+app.use(express.json());
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/events', require('./routes/events'))
+//TODO: CRUD: Eventos
+
+
+//Escuchar Peticiones
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
+})
